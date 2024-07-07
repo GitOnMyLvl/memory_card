dfd62be340f33d61bd62e2be80fc8314375c75c0
Wolfgang Vinatzer <44369896+GitOnMyLvl@users.noreply.github.com>
2024-07-07 15:24:43 +0200

    add loading display

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,14 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import fetchData from "./fetchData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+
 
 function Card( { pokemonNumber }){
   const [pokemonName, setPokemonName] = useState("");
   const [pokemonImage, setPokemonImage] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     let ignore = false;
@@ -13,6 +17,7 @@ function Card( { pokemonNumber }){
         console.log('Fetching data');
         setPokemonName(result.name);
         setPokemonImage(result.image);
+        setIsLoading(false)
       }
     });
     return () => {
@@ -22,9 +27,21 @@ function Card( { pokemonNumber }){
 
   return(
     <div className="card">
-      <button type="button" className="clickable-card"> 
-        <h3>{ pokemonName }</h3>
-        <img src={pokemonImage} alt={pokemonName} />
+      <button type="button" className="clickable-card">
+        {isLoading ? (
+          <>
+            <p>loading</p>
+            <FontAwesomeIcon icon={faSpinner} spin/>
+          </>
+        ):(
+          <>
+            <p>{ pokemonName }</p>
+            <img src={pokemonImage} alt={pokemonName} />
+          </>
+        )
+        
+        } 
+       
       </button>
     </div>
   )

bdf9e4114cd0b409315d43c83f2307a10ed6fba9
Wolfgang Vinatzer <44369896+GitOnMyLvl@users.noreply.github.com>
2024-07-07 13:13:34 +0200

    add card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+function Card( { pokemonNumber }){
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonImage, setPokemonImage] = useState("");
+
+  useEffect(() => {
+    let ignore = false;
+    fetchData(pokemonNumber).then(result => {
+      if (!ignore){
+        console.log('Fetching data');
+        setPokemonName(result.name);
+        setPokemonImage(result.image);
+      }
+    });
+    return () => {
+      ignore = true;
+    }
+  }, [pokemonNumber]);
+
+  return(
+    <div className="card">
+      <button type="button" className="clickable-card"> 
+        <h3>{ pokemonName }</h3>
+        <img src={pokemonImage} alt={pokemonName} />
+      </button>
+    </div>
+  )
+}
+
+export default Card;

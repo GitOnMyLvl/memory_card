9f3bb679580a48e1f3087e7bf739434e05aafffc
Wolfgang Vinatzer <44369896+GitOnMyLvl@users.noreply.github.com>
2024-07-08 08:40:31 +0200

    add basic info text

---
+++ src/components/Info.jsx
@@ -0,0 +1,11 @@
+function Info() {
+  return (
+    <div className="info">
+      <h1>Info</h1>
+      <p>Click on a card and remember the Pokemon.<br></br>
+         If you click on the same Pokemon twice, you lose.<br></br>
+         Try to beat the High Score
+      </p>
+    </div>
+  );
+}

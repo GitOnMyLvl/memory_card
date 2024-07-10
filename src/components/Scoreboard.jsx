4d9d8e937f490629ac6d7ddca49964cd2646520f
Wolfgang Vinatzer <44369896+GitOnMyLvl@users.noreply.github.com>
2024-07-10 15:58:48 +0200

    small layout change

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,8 +1,8 @@
 function Scoreboard( {currentScore, highScore}) {
   return (
     <div className="scoreboard">
-      <p>Current Score: {currentScore}</p>
-      <p>High Score: {highScore}</p>
+      <p className="score">Current Score: <span>{currentScore}</span></p>
+      <p className="score">High Score: <span>{highScore}</span></p>
     </div>
   );
 }
